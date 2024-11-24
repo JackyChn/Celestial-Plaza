343760b45543432df7ea16f634a48be20223bf05
JackyChn <ccc1020048076@gmail.com>
2024-11-24 15:29:29 +1030

    Update: designed folder sturcture added

---
+++ src/app/collections/[slug]/layout.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CollectionsLayout() {
+  return <div>CollectionsLayout</div>;
+}
---
+++ src/app/shop/layout.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ShopLayout() {
+  return <div>ShopLayout</div>;
+}
