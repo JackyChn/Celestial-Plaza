d0e4f93aef2d327f2ca325901466d14898039c78
JackyChn <ccc1020048076@gmail.com>
2024-11-24 15:24:31 +1030

    Update: title  and subtitle style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,32 @@
 import Image from "next/image";
 import Banner from "../../public/banner.jpg";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
-
 import { ArrowRight } from "lucide-react";
 import { LitupBorderButton } from "@/components/ui/LitupBorderButton";
+import { TextGenerateEffect } from "@/components/ui/TextGenerateEffect";
+import { TextRevealCard } from "@/components/ui/TextRevealCard";
 
 export default function Home() {
   return (
     <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
-      <div className="flex items-center bg-secondary md:h-96">
+      <div className="flex items-center bg-blue-100 bg-secondary md:h-96">
         {/* text & button */}
         <div className="flex flex-col items-center justify-center space-y-7 p-10 text-center md:w-1/2">
-          <h1>Fill the Gap of your heart</h1>
-          <p>
-            Feeling alone? Wallet light? Treat yourself to something extravagant
-            and turn it all around!
-          </p>
-          <LitupBorderButton>
+          {/* <h1 className="text-3xl font-bold md:text-4xl">
+            Fill the Gap of your heart
+          </h1> */}
+          <TextRevealCard
+            text="Wanna do some shopping?"
+            revealText="Fill the Gap of your heart!"
+          />
+          <TextGenerateEffect
+            className=""
+            words="Feeling alone? Wallet light? Treat yourself to something extravagant
+            and turn it all around!"
+            duration={1}
+          />
+
+          <LitupBorderButton otherClasses="bg-gradient-to-r from-cyan-500 to-blue-500 ">
             <Link
               href={"/shop"}
               className="flex items-center justify-center gap-2"
@@ -29,12 +38,13 @@ export default function Home() {
         </div>
 
         {/* image on the right */}
-        <div className="hidden h-full w-1/2 overflow-hidden md:block">
+        <div className="relative hidden h-full w-1/2 overflow-hidden md:block">
           <Image
             src={Banner}
             alt="banner"
             className="h-full w-full object-cover"
           />
+          <div className="absolute inset-0 bg-gradient-to-r from-blue-100 via-transparent to-transparent" />
         </div>
       </div>
     </main>
--- src/components/ui/LitupBorderButton.tsx
+++ src/components/ui/LitupBorderButton.tsx
@@ -2,13 +2,15 @@ import React from "react";
 
 export const LitupBorderButton = ({
   children,
+  otherClasses,
 }: {
   children?: React.ReactNode;
-  handleClick?: () => void;
   otherClasses?: string;
 }) => {
   return (
-    <div className="group relative flex h-full w-[18vh] rounded-full bg-blue-600 px-6 py-1 text-white transition duration-200 hover:bg-blue-400">
+    <div
+      className={`${otherClasses} group relative flex h-full w-[18vh] rounded-full bg-blue-600 px-6 py-1 text-white transition duration-200 hover:bg-blue-400`}
+    >
       {children}
     </div>
   );
