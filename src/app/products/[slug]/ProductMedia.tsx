49bf7f009c4471c71e4ff8eed15e54257c5b8caf
JackyChn <ccc1020048076@gmail.com>
2024-11-27 15:27:53 +1030

    Update: ProductsMedia.tsx to show image and video at the same time and zoom in/out the image

--- src/app/products/[slug]/ProductMedia.tsx
+++ src/app/products/[slug]/ProductMedia.tsx
@@ -1,10 +1,19 @@
 // Render all images/videos
 import WixImage from "@/components/WixImage";
+import { cn } from "@/lib/utils";
 import { products } from "@wix/stores";
+import { PlayIcon } from "lucide-react";
 import React, { useState } from "react";
+import Zoom from "react-medium-image-zoom";
 
 interface ProductMediaProps {
   media: products.MediaItem[] | undefined;
+}
+
+interface MediaPreviewProps {
+  mediaItem: products.MediaItem;
+  isSelected: boolean;
+  onSelect: () => void;
 }
 
 export default function ProductMedia({ media }: ProductMediaProps) {
@@ -15,15 +24,18 @@ export default function ProductMedia({ media }: ProductMediaProps) {
   const selectedVideo = selectedMedia?.video?.files?.[0];
 
   return (
-    <div className="basis-2/5">
+    <div className="h-fit basis-2/5 space-y-5 md:sticky md:top-0">
+      {/* show image if exsits */}
       <div className="aspect-square bg-secondary">
         {selectedImage?.url ? (
-          <WixImage
-            mediaIdentifier={selectedImage.url}
-            alt={selectedImage.altText}
-            width={1000}
-            height={1000}
-          />
+          <Zoom key={selectedImage.url}>
+            <WixImage
+              mediaIdentifier={selectedImage.url}
+              alt={selectedImage.altText}
+              width={1000}
+              height={1000}
+            />
+          </Zoom>
         ) : selectedVideo?.url ? (
           <div className="flex size-full items-center bg-black-100">
             <video controls className="size-full">
@@ -35,6 +47,58 @@ export default function ProductMedia({ media }: ProductMediaProps) {
           </div>
         ) : null}
       </div>
+
+      {/* show video if exsits */}
+      {media.length > 1 && (
+        <div className="mt-5 flex flex-wrap gap-5">
+          {media.map((mediaItem) => (
+            <MediaPreview
+              key={mediaItem._id}
+              mediaItem={mediaItem}
+              isSelected={mediaItem._id === selectedMedia?._id}
+              onSelect={() => setSelectedMedia(mediaItem)}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+function MediaPreview({ mediaItem, isSelected, onSelect }: MediaPreviewProps) {
+  /*
+    The thing we're doing here is to show the rest of media including images and videos, the logic is to get the 
+    stillFrameMediaId and put it to thumbnail url and discard the /v1/fit... which specify the pixcel
+  */
+  const imageUrl = mediaItem.image?.url;
+  const stillFrameMediaId = mediaItem.video?.stillFrameMediaId;
+  const thumbnailUrl = mediaItem.thumbnail?.url;
+  const resolvedThumbnailUrl =
+    stillFrameMediaId && thumbnailUrl
+      ? thumbnailUrl.split(stillFrameMediaId)[0] + stillFrameMediaId
+      : undefined;
+
+  if (!imageUrl && !resolvedThumbnailUrl) return null;
+
+  return (
+    <div
+      className={cn(
+        "relative cursor-pointer bg-secondary",
+        isSelected && "outline outline-1 outline-primary",
+      )}
+    >
+      <WixImage
+        mediaIdentifier={imageUrl || resolvedThumbnailUrl}
+        alt={mediaItem.image?.altText || mediaItem.video?.files?.[0].altText}
+        width={100}
+        height={100}
+        onMouseEnter={onSelect}
+      />
+      {resolvedThumbnailUrl && (
+        <span className="absolute left-1/2 top-1/2 flex size-9 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-black/40">
+          <PlayIcon className="size-5 text-white/60" />
+        </span>
+      )}
     </div>
   );
 }
